58d89f42b84a080388981f1fe45ebdd25dae9c8d
Ghazal Pouresfandiyar <ghazal.brn@gmail.com>
2021-07-03 11:22:50 +0430

    facing simulation bug

--- Code/cpu.v
+++ Code/cpu.v
@@ -1,15 +1,27 @@
-module CPU(clk);
+module CPU(clk, AC_input, PC_input, IR_input);
 	input clk;
+	//input [7:0]M_input[0:15];
+	input [7:0]AC_input; 
+	input [3:0]PC_input;
+	input [7:0]IR_input;
+	//define regs for inputs
 	reg [7:0]M[0:15];
 	reg [7:0]AC; 
 	reg [3:0]PC;
+	reg [7:0]IR;
+	//auxiliary regs
 	reg [2:0]SC;
 	reg [3:0]AR;
-	reg [7:0]IR;
 	reg [2:0]opcode;
 	reg I; 
 	reg IR0, IR1, IR2, IR3, IR4, IR5, IR6, IR7;
+	//assignments
 	assign SC = 3'b000;
+	//assign M = M_input;
+	assign AC = AC_input;
+	assign PC = PC_input;
+	assign IR = IR_input;
+
 	always @ (posedge clk)
 	begin 
 		case(SC)
@@ -81,15 +93,26 @@ endmodule
 `include "Parameter.v"
 module test_bench;
 	reg clk;
-	CPU cpu(.clk(clk));
+	reg [7:0] AC;
+	reg [7:0] PC;
+	reg [7:0] IR;
+
 
 	initial
 	begin
 	clk <= 0;
 	`simulation_time;
    	$finish;
+	AC = 8'b00000000;
+	PC = 8'b00000000;
+	IR = 01100000; //cmp instruction, direct addressing in address 0000
 	end
 	
+	CPU cpu(
+		.clk(clk),
+		.AC(AC),
+		.PC(PC),
+		.IR(IR));
 	always
 	begin
 		#5 clk = ~clk;
